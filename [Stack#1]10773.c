aee3bb51e5a5e9412618462b1d601528c0febd57
KimDaeHyun99 <86544773+KimDaeHyun99@users.noreply.github.com>
2022-05-01 01:55:44 +0900

    Update [Stack#1]10773.c

--- [Stack#1]10773.c
+++ [Stack#1]10773.c
@@ -22,34 +22,7 @@ int main()
             stack[count--] = 0;
         }
     }
-    for (int i = 0; i < K; i++)#include <stdio.h>
-int K = 0;
-int count = -1;
-int input[100000];
-int stack[100000];
-int sum = 0;
-
-int main()
-{
-    scanf_s("%d", &K);
-
     for (int i = 0; i < K; i++)
-    {
-        scanf_s("%d", &input[i]);
-        if (input[i])
-        {
-            count++;
-            stack[count] = input[i];
-        }
-        else
-        {
-            stack[count--] = 0;
-        }
-    }
-    for (int i = 0; i < K; i++)
-        sum += stack[i];
-    printf("%d", sum);
-}
         sum += stack[i];
     printf("%d", sum);
 }
