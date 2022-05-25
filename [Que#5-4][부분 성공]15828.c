944ff0e9db85a5997db50097fc8a01b1d2d54eba
KimDaeHyun99 <86544773+KimDaeHyun99@users.noreply.github.com>
2022-05-25 23:25:40 +0900

    Create [Que#5-4][부분 성공]15828.c

---
+++ [Que#5-4][부분 성공]15828.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int main()
+{
+    int N = 0;
+    int N_count = 0;
+    int j = -1;
+    int k = 0;
+
+    scanf("%d", &N);
+
+    int input[100000];
+    int q[100000];
+
+    for (int i = 0; i < 100000; i++)
+        q[i] = 0;
+    for (int i = 0; input[i - 1] != -1; i++)
+    {
+
+        scanf("%d", &input[i]);
+    }
+    for (int i = 0; input[i - 1] != -1; i++)
+    {
+        if (!N_count)
+        {
+            j = -1;
+            k = 0;
+        }
+        if ((k >= N) && (N - 1 > N_count))
+            k = 0;
+        if ((j >= N) && (N - 1 > N_count))
+            j = -1;
+
+        if (input[i] == 0 && N_count)
+        {
+            j++;
+            q[j] = 0;
+            N_count--;
+            if (N_count < 0)
+                N_count = 0;
+        }
+        else if (input[i] == -1)
+            break;
+        else if ((input[i] > 0) && (N > N_count) && (N_count >= 0))
+        {
+            if (!q[k])
+            {
+                q[k] = input[i];
+                k++;
+                N_count++;
+            }
+        }
+    }
+
+    if (N_count <= 0)
+        printf("empty\n");
+
+    else
+    {
+        for (int i = 0; i < N; i++)
+        {
+
+            // if (j>=N)
+            //   j=0;
+            // if (q[j])
+            //{
+            if (j + 1 == N)
+                j = -1;
+            if (q[j + 1])
+                printf("%d\n", q[++j]);
+            else
+                j++;
+            // j++;
+            //}
+        }
+    }
+}
